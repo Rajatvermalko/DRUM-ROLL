de69e41a42f83fbfe0e77c582af1bc5975e9f958
Rajat Verma <rvrajatverma773@gmail.com>
2022-06-27 20:26:48 +0530

    feat sound

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-alert("hello");
+var value=document.querySelectorAll(".drum").length;
+for(var i=0;i<value;i++)
+{
+document.querySelectorAll(".drum")[i].addEventListener("click",handleclick);
+function name() {
+var valuechecker=this.innerHtml;
+switch (valuechecker) {
+    case "w":
+        var audio=new Audio("sounds/tom-z.mp3");
+        audio.play();
+        break;
+
+    default:
+        break;
+}
+}
+}
